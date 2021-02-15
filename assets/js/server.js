e8aae379a4edfdcbb2f1bc10b3083a7ec7079f9e
Joseph Soria <josephsoria6@gmail.com>
2021-02-14 22:07:21 -0600

    Modified file structure, began scripting server and routing files

---
+++ assets/js/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 7000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// require(`../../routes/apiRouting`)(app);
+require(`../../routes/htmlRouting`)(app);
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running, listening on: ${PORT}`);
+})
