a95a7b38547c974ab6ae7e00a4bc35f58ecda736
Joseph Soria <josephsoria6@gmail.com>
2021-02-17 23:03:20 -0600

    Added middleware to serve index.js, changed script link in notes.html

--- assets/js/server.js
+++ assets/js/server.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 7000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'../css')));
+app.use(express.static(__dirname));
+
 
 require(`../../routes/apiRouting`)(app);
 require(`../../routes/htmlRouting`)(app);
