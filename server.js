fc022e8bb36894a0d8264f4edcdb784352d4f3d4
Joseph Soria <josephsoria6@gmail.com>
2021-02-21 13:02:43 -0600

    Changed module paths for routing

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const PORT = process.env.PORT || 7000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'../css')));
+app.use(express.static(path.join(__dirname,'./assets/css')));
 app.use(express.static(__dirname));
 
 
-require(`../../routes/apiRouting`)(app);
-require(`../../routes/htmlRouting`)(app);
+require(`./routes/apiRouting`)(app);
+require(`./routes/htmlRouting`)(app);
 
 app.listen(PORT, () => {
     console.log(`Server is up and running, listening on: ${PORT}`);
